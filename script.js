ba8e1428e77375fb5f1ed38ca6c31d40fd96cfcc
Superfelicia <felicia.ahlstrom@hotmail.se>
2021-12-10 13:44:27 +0100

    code

--- script.js
+++ script.js
@@ -1,32 +1,82 @@
 // BANK ACCOUNT
 
-// Start by creating the object and the properties. You can leave the functions empty for now if you want.
-// You'll find all the information about what properties the object should have in the readme file.
+// You should also do some research why parseFloat() is used here.
 
-// In the deposit and withdrawal function use this code to show the prompt
-parseFloat(prompt("Put some proper message here"));
-// You need to change the message of course. You should also do some research why parseFloat() is used here
-// answer by making a comment in the code.
-// Be aware of that you need to add some code to this so you can display the actual input.
+// parseFloat() will return NaN if it cannot convert the first character
+// it will ignore white spaces in between and after a sentence or number
+// it will only return the first number in a string and ignore the rest
+// it is required a value to parse (?)
 
-// In the deposit and withdrawal function you also need to handle som potential errors.
-// To handle one of the potential error you can use this piece of code
-isNaN(variableName);
-// you need to change the variableName to your actual variable.
-// you also need to check some other conditions here and use a logical operator.
-// try to say the sentece out loud or think it:
-// "A user should not be able to put 0 or negative numbers or empty string or a type that is not a number"
-// Do you know what operator you should use?
 
-// In the accountError and accountExit function you can use this to show a message to the user:
-alert("your code here and maybe properties");
-// of course you need to put a proper message and you might need to display some of the properties.
+// OBJECT
+const account = {
+  accountName: "Felicia Ahlström",
+  balance: 100,
+  getBalance: function () {
+    alert(`Current balance: ${this.balance}`);
+    atm();
+    // atm() makes the starter function run again, so that we go back to choice menu
+  },
+  deposit: function () {
+    let depositAmount = parseFloat(prompt("How much would you like to deposit?"));
+    if (depositAmount <= 0 || isNaN(depositAmount)) {
+      this.accountError();
+      this.deposit();
+      // this.deposit makes the function run again
+    } else {
+      this.balance = this.balance + depositAmount;
+      alert(`
+      Deposited amount: ${depositAmount}`);
+      this.getBalance();
+    }
+  },
+  withdrawal: function () {
+    let withdrawalAmount = parseFloat(prompt("How much would you like to withdraw?"));
+    if (withdrawalAmount <= 0 || isNaN(withdrawalAmount)) {
+      this.accountError();
+      this.withdrawal();
+    } else {
+      this.balance = this.balance - withdrawalAmount;
+      alert(`Withdrawn amount: ${withdrawalAmount}`);
+      this.getBalance();
+    }
+  },
+  getAccountName: function () {
+    alert(`Name of account: ${this.accountName}`);
+    atm();
+  },
+  accountError: function () {
+    alert("The input is not valid")
+
+    // display a message to the user when there has been an error.
+    // I have put an if/else statement in both deposit and withdrawal
+    // to handle possible errors. Maybe I can call for accountError instead?
+  },
+  exitAccount: function () {
+    window.close();
+  }
+}
 
 // STARTER FUNCTION
 function atm() {
-  let choice = parseInt(prompt("Display menu choices here"));
-  //prompt user for choice.
+  let choice = parseInt(prompt("Select a choice 1.) See balance 2.) Make a deposit 3.) Make a withdrawal 4.) Get account name 5. Exit"));
 
-  // you can use the variable choice for your switch or if/else statement
-  // so choice will hold the value of the user input.
+  switch (choice) {
+    case 1: account.getBalance();
+    break;
+    case 2: account.deposit();
+    break;
+    case 3: account.withdrawal();
+    break;
+    case 4: account.getAccountName();
+    break;
+    case 5: account.exitAccount();
+    break;
+    default: account.accountError();
+    atm();
+    break;
+  }
 }
+
+// DISPLAY ACCOUNT BALANCE
+  atm();
